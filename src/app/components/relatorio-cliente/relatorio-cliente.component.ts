c1b9492aab52d1c20145e2dd232c222a0c6f6659
Matheus Herculano <matheus95df@gmail.com>
2023-07-16 17:54:44 -0300

    gerando link publico

--- src/app/components/relatorio-cliente/relatorio-cliente.component.ts
+++ src/app/components/relatorio-cliente/relatorio-cliente.component.ts
@@ -1,8 +1,9 @@
-import { CommonModule } from "@angular/common";
+import { CommonModule, LocationStrategy } from "@angular/common";
 import { Component, Input, OnInit } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { ActivatedRoute } from "@angular/router";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { ActivatedRoute, Router  } from "@angular/router";
 import * as _ from "lodash";
 import { MaterialModule } from "src/app/app.module";
 import { ClienteService } from "src/app/core/services/cliente.service";
@@ -22,6 +23,7 @@ interface ComboBox {
 export class RelatorioClienteComponent implements OnInit {
   @Input() showFilters: boolean;
 
+  idCliente = this.activatedRoute.snapshot.params["idCliente"];
   cliente = {
     nomeEmpresa:"",
     metric:{
@@ -34,34 +36,75 @@ export class RelatorioClienteComponent implements OnInit {
   };
   periodoSelecionado = "TODAY";
   periodoList: ComboBox[] = [];
+  linkPublicRelatorioCliente = "asd";
+  urlCliente = "";
 
   constructor(
     private clienteService: ClienteService,
     private elementosTelaService: ElementosTelaService,
-    private route: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private locationStrategy: LocationStrategy,
+    private _snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
-    this.carregarDados();
+    this.carregarDados(this.periodoSelecionado);
     this.carregarCombos();
+    
+    
   }
 
-  private carregarDados() {
-    var idCliente = this.route.snapshot.params["idCliente"];
-    if(idCliente != null){
-      this.clienteService.getClienteById(idCliente).subscribe((data) => {
-        this.cliente = buildCliente(data)
-  
-        if (data["adwords"] != "") {
-          var costumerId = data["adwords"].replace(/-/g, ""); //removendo os - "traço"
-  
-          this.clienteService.getMetrics(costumerId).subscribe((metric) => {
-            this.cliente.metric = buildMetric(metric);;
-          });
-        }
-      });
+  public copyText(urlCliente) {
+    navigator.clipboard.writeText(urlCliente);
+    this._snackBar.open("Link copiado!", "Ok")
+  }
+
+  private obterUrlQueryParams(){
+    var queryParams = {
+      periodo: null,
+      public: false
+    };
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      queryParams.periodo = params['periodo'];
+      queryParams.public = params['public'];
+    });
+
+    return queryParams;
+  }
+
+  private carregarDados(periodo) {
+    if(this.idCliente != null){
+      if(this.obterUrlQueryParams().public){ //decidindo se pega o periodo da variavel de URL em caso de link public ou não
+        this.requisicoesHTTP(this.idCliente, this.obterUrlQueryParams().periodo);
+      }else{
+        this.requisicoesHTTP(this.idCliente, periodo);
+      }
     }
-    
+    this.montarUrlCliente(periodo);
+
+  }
+
+  private montarUrlCliente(periodo){
+    var currentUrl =  window.location.href;
+    currentUrl = currentUrl.split('#')[0]; //removendo activeRoute atual
+    currentUrl = currentUrl + "#/public/relatorio-cliente/"+this.idCliente+"?periodo="+periodo+"&public=true";
+    this.urlCliente = currentUrl;
+  }
+
+  private requisicoesHTTP(idCliente, periodo) {
+    this.clienteService.getClienteById(idCliente).subscribe((data) => {
+      this.cliente = buildCliente(data);
+
+      if (data["adwords"] != "") {
+        var costumerId = data["adwords"].replace(/-/g, ""); //removendo os - "traço"
+
+        this.clienteService.getMetrics(costumerId, periodo).subscribe((metric) => {
+          this.cliente.metric = buildMetric(metric);;
+        });
+      }
+    });
   }
 
   private carregarCombos(){
