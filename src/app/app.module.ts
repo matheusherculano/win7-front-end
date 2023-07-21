210910208833e377ef09269c72d860afe70a9b33
Matheus Herculano <matheus95df@gmail.com>
2023-07-21 09:45:21 -0300

    inclusão de mascara monetaria e separador de milheiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,60 +1,60 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { APP_BASE_HREF, registerLocaleData } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { LOCALE_ID, NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule } from "@angular/router";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { APP_BASE_HREF, registerLocaleData } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule} from '@angular/material/card';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatIconModule} from '@angular/material/icon';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatTableModule} from '@angular/material/table';
-import {MatSortModule} from '@angular/material/sort';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSliderModule } from "@angular/material/slider";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatListModule } from "@angular/material/list";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatIconModule } from "@angular/material/icon";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatTableModule } from "@angular/material/table";
+import { MatSortModule } from "@angular/material/sort";
+import { MatPaginatorModule } from "@angular/material/paginator";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-import { SidebarModule } from './sidebar/sidebar.module';
-import { FooterModule } from './shared/footer/footer.module';
-import { NavbarModule} from './shared/navbar/navbar.module';
-import { FixedpluginModule} from './shared/fixedplugin/fixedplugin.module';
-import { AdminLayoutComponent } from './layouts/admin/admin-layout.component';
-import { AuthLayoutComponent } from './layouts/auth/auth-layout.component';
+import { SidebarModule } from "./sidebar/sidebar.module";
+import { FooterModule } from "./shared/footer/footer.module";
+import { NavbarModule } from "./shared/navbar/navbar.module";
+import { FixedpluginModule } from "./shared/fixedplugin/fixedplugin.module";
+import { AdminLayoutComponent } from "./layouts/admin/admin-layout.component";
+import { AuthLayoutComponent } from "./layouts/auth/auth-layout.component";
 
-import { AppRoutes } from './app.routing';
-import { LoadingComponent } from './core/components/loading/loading.component';
-import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
-import ptBr from '@angular/common/locales/pt';
+import { AppRoutes } from "./app.routing";
+import { LoadingComponent } from "./core/components/loading/loading.component";
+import { LoadingInterceptor } from "./core/interceptors/loading.interceptor";
+import ptBr from "@angular/common/locales/pt";
 
-import { MAT_DATE_LOCALE} from '@angular/material/core';
+import { MAT_DATE_LOCALE } from "@angular/material/core";
 
-registerLocaleData(ptBr)
+registerLocaleData(ptBr);
 
 @NgModule({
   exports: [
@@ -87,44 +87,43 @@ registerLocaleData(ptBr)
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
-    MatNativeDateModule
+    MatNativeDateModule,
   ],
-  declarations: [
-  
-  ]
+  declarations: [],
 })
 export class MaterialModule {}
 
 @NgModule({
-    imports:      [
-        CommonModule,
-        BrowserAnimationsModule,
-        FormsModule,
-        RouterModule.forRoot(AppRoutes,{
-          useHash: true
-        }),
-        HttpClientModule,
-        MaterialModule,
-        SidebarModule,
-        NavbarModule,
-        FooterModule,
-        FixedpluginModule
-    ],
-    declarations: [
-        AppComponent,
-        AdminLayoutComponent,
-        AuthLayoutComponent,
-        LoadingComponent,
-        
-    ],
-    providers : [
-      MatNativeDateModule,
-      {
-        provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true
-      },
-      { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' }
-  
-    ],
-    bootstrap:    [ AppComponent ]
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule.forRoot(AppRoutes, {
+      useHash: true,
+    }),
+    HttpClientModule,
+    MaterialModule,
+    SidebarModule,
+    NavbarModule,
+    FooterModule,
+    FixedpluginModule,
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent,
+    AuthLayoutComponent,
+    LoadingComponent,
+  ],
+  providers: [
+    MatNativeDateModule,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
+      multi: true,
+    },
+    { provide: MAT_DATE_LOCALE, useValue: "pt-BR" },
+    { provide: LOCALE_ID, useValue: "pt" },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
